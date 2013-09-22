209dbabd931b2f1eb2796c045e42cad7e3e0fe06
Severin Mueller <severin.mueller@gmx.net>
2013-09-22 22:57:49 +0200

    updates

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <?php
 
     $recipient = "severin.mueller@gmx.net";
-    $header = 'From: adolf@h1tl3r.com' . "\r\n" .
-    'Reply-To: adolf@h1tl3r.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
     $subject = "fourteen words";
     $body = "Adolf H, superstar!!!";
     
-     if (mail($recipient, $subject, $body, $header)) {
+     if (mail($recipient, $subject, $body)) {
          echo("<p>Mail erfolgreich gesendet</p>");
      } else {
          echo("<p>Beim Senden der Mail ist ein Fehler aufgetreten");
