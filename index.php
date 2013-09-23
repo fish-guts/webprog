b479a323f76fc363c9b7bf9902a9bb5a56b9b8bc
fish-guts <severin.mueller@gmx.net>
2013-09-23 15:12:49 +0200

    added mail headers

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
 
+    $header = 'From: severin.mueller@gmx.net' . "\r\n" .
+    'Reply-To: severin.mueller@gmx.net' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
     $recipient = "xobe@zhaw.ch";
     $subject = "Test E-Mail";
-    $body = "Hoi Jaim\n\n, das ist meine Test E-Mail, hoffentlich kommts an. ";
+    $body = "Hoi Jaime\n\n, das ist meine Test E-Mail, hoffentlich kommts an. ";
     
-     if (mail($recipient, $subject, $body)) {
+     if (mail($recipient, $subject, $body, $header)) {
          echo("<p>Mail erfolgreich gesendet</p>");
      } else {
          echo("<p>Beim Senden der Mail ist ein Fehler aufgetreten");
