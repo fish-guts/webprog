5ab625aafcaf31513fa7fbb5929e2b22291a33db
Severin Mueller <severin.mueller@gmx.net>
2013-09-23 20:58:05 +0200

    removed redunant from var

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
     /* smtp über postfix für gmail konfiguriert (mit tls) */
-    $from = 'severinmueller1983@gmail.com';
-    /* emfpänger */
     $to = 'xobe@zhaw.ch';
     $subject = 'Hi!';
     $body = "Hi Jaime,\n\nDies ist eine TestMail, generiert von meinem PHP Script gesendet mit postfix\n\nGruess Severin";
 
     $header = 'From: Severin Müller' . "\r\n" .
-    'Reply-To: severinmueller1983@gmail.com' . "\r\n" .
+    'Reply-To: severinmueller1983@gmail,com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
 
